5d48880cbe7025e44abdd83face3026782df66cc
Andrey <kouznts@ya.ru>
2019-12-22 16:28:30 +0400

    описать модель Warehouse

---
+++ HotelWebApplication/Models/Warehouse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelWebApplication.Models
+{
+    public class Warehouse
+    {
+        [Display(Name = "Номер товара")]
+        [Key] public int ItemId { get; set; }
+
+        [Display(Name = "Название")]
+        public string ItemName { get; set; }
+
+        [Display(Name = "Цена")]
+        public int ItemPrice { get; set; }
+
+        [Display(Name = "Поставщик")]
+        public string SupplierName { get; set; }
+
+        [Display(Name = "В наличии")]
+        public bool IsAvailable { get; set; }
+    }
+}
