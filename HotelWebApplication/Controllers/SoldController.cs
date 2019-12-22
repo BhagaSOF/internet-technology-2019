28b1d2666d29f1a21baf6d333c4d8be9079ba3c7
Andrey <kouznts@ya.ru>
2019-12-22 17:28:37 +0400

    описать SoldController

---
+++ HotelWebApplication/Controllers/SoldController.cs
@@ -0,0 +1,26 @@
+﻿using HotelWebApplication.Dal;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace HotelWebApplication.Controllers
+{
+    public class SoldController : Controller
+    {
+        private WarehouseContext db = new WarehouseContext();
+
+        [Authorize()]
+        public ActionResult Index()
+        {
+            return RedirectToAction("All", "Sold");
+        }
+
+        [Authorize()]
+        public ActionResult All()
+        {
+            var goodsForSale = db.Goods.Where(g => g.IsAvailable == false);
+            ViewBag.GoodsForSale = goodsForSale;
+
+            return View();
+        }
+    }
+}
