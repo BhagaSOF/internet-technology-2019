8c64c1db10b0a7b7dbf48b1bb82d2d277c4e7c3c
Andrey <kouznts@ya.ru>
2019-12-22 16:38:42 +0400

    рефакторинг WarehouseContext

--- HotelWebApplication/Dal/WarehouseContext.cs
+++ HotelWebApplication/Dal/WarehouseContext.cs
@@ -6,9 +6,6 @@ namespace HotelWebApplication.Dal
     public class WarehouseContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-
-        public DbSet<Item> Bookings { get; set; }
-        public DbSet<Client> Clients { get; set; }
-        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Item> Items { get; set; }
     }
 }
