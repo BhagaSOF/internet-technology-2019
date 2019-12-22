ce4129f2a0d7608d149aba130a7c1d429fa3a6f0
Bhaga <ionovandrew@gmai.com>
2019-12-22 18:04:24 +0400

    добавил записи товаров

--- HotelWebApplication/Dal/WarehouseDbInitializer.cs
+++ HotelWebApplication/Dal/WarehouseDbInitializer.cs
@@ -13,6 +13,86 @@ namespace HotelWebApplication.Models
                 Password = "password"
             });
 
+            db.Goods.Add(new Good
+            {
+                Name = "Vodka",
+                Price = 600,
+                Supplier = "Five lakes",
+                IsAvailable = true
+            });
+
+            db.Goods.Add(new Good
+            {
+                Name = "Beer 4.7%",
+                Price = 120,
+                Supplier = "Tyxtin",
+                IsAvailable = true
+            });
+
+            db.Goods.Add(new Good
+            {
+                Name = "Beer 3.0%",
+                Price = 80,
+                Supplier = "Baltika",
+                IsAvailable = false
+            });
+
+            db.Goods.Add(new Good
+            {
+                Name = "Double Liker",
+                Price = 1200,
+                Supplier = "Sheridans",
+                IsAvailable = true
+            });
+
+            db.Goods.Add(new Good
+            {
+                Name = "Samogon",
+                Price = 800,
+                Supplier = "Uncle grandpa",
+                IsAvailable = true
+            });
+
+            db.Goods.Add(new Good
+            {
+                Name = "Cognac",
+                Price = 1600,
+                Supplier = "Hennessy",
+                IsAvailable = false
+            });
+
+            db.Goods.Add(new Good
+            {
+                Name = "Beer 6.0%",
+                Price = 600,
+                Supplier = "Guinness",
+                IsAvailable = true
+            });
+
+            db.Goods.Add(new Good
+            {
+                Name = "Cider",
+                Price = 75,
+                Supplier = "Apples company",
+                IsAvailable = false
+            });
+
+            db.Goods.Add(new Good
+            {
+                Name = "Tequila",
+                Price = 1300,
+                Supplier = "1800 silver",
+                IsAvailable = true
+            });
+
+            db.Goods.Add(new Good
+            {
+                Name = "Whiskey",
+                Price = 800,
+                Supplier = "Jack Daniels",
+                IsAvailable = false
+            });
+
 
             base.Seed(db);
         }
