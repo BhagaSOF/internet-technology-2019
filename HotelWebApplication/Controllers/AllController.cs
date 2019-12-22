dc53e7fcdf64ab8925e9918b9ea4130e4ebaf078
Andrey <kouznts@ya.ru>
2019-12-22 17:34:07 +0400

    описать отображение всех товаров (те, что в наличии и что проданные)

---
+++ HotelWebApplication/Controllers/AllController.cs
@@ -0,0 +1,27 @@
+﻿using HotelWebApplication.Dal;
+using HotelWebApplication.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace HotelWebApplication.Controllers
+{
+    public class AllController : Controller
+    {
+        private WarehouseContext db = new WarehouseContext();
+
+        [Authorize()]
+        public ActionResult Index()
+        {
+            return RedirectToAction("All", "All");
+        }
+
+        [Authorize()]
+        public ActionResult All()
+        {
+            IEnumerable<Good> goods = db.Goods;
+            ViewBag.Goods = goods;
+
+            return View();
+        }
+    }
+}
