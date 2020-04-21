8ed3733eeb2718ce7332b9ee5f4ff0c916442c05
Andrey <kouznts@ya.ru>
2020-04-21 16:58:52 +0400

    FindContoller and its views

---
+++ Webapp/Controllers/FindController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Webapp.Dal;
+using Webapp.Models;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Webapp.Controllers
+{
+    public class FindController
+    {
+        private readonly WarehouseContext db = new WarehouseContext();
+
+        [Authorize()]
+        public ActionResult Index()
+        {
+            return RedirectToAction("All", "Find");
+        }
+
+        [Authorize()]
+        public ActionResult All()
+        {
+            IEnumerable<Good> goods = db.Goods;
+            ViewBag.Goods = goods;
+
+            return View();
+        }
+        
+        [HttpPost]
+        [Authorize()]
+        public ActionResult Search(string search)
+        {
+            IEnumerable<Good> goods = db.Goods.Where(
+                r => r.Name.Contains(search) ||
+                     r.Supplier.Contains(search)).ToList();
+
+            return PartialView(goods);
+        }
+    }
+}
